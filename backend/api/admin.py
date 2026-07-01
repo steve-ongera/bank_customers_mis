ad9700283bc62fd6bd83e185a2716325f2fd28d5
steve-ongera <gadafisteve001@gmail.com>
2026-07-01 15:07:52 +0300

    registred my models in AdminPanel

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display= ['account_number','first_name', 'last_name','email', 'account_balance']
+    search_fields = ['account_number', 'email', 'id_number']
+    readonly_fields=['account_number', 'created_at', 'updated_at']
