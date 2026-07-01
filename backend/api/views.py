c6d8f209007c8daf59ff584c4f2515e66c78bb5e
steve-ongera <gadafisteve001@gmail.com>
2026-07-01 14:40:07 +0300

    created a business logic to list and create customers

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Customer
+from .serializers import CustomerSerializer
 
-# Create your views here.
+
+class CustomerListCreateView(generics.ListCreateAPIView):
+    """"
+    GET: List all customers
+    POST: Create a new customer
+    """
+    queryset= Customer.objects.all()
+    serializer_class = CustomerSerializer
